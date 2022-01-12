f13fd77331c23f77fc3c02a18b13bb0d8aeca300
Chris <dermentzoglou.chris@gmail.com>
2022-01-11 19:26:04 -0500

    First Commit

---
+++ src/main/cpp/Robot.cpp
@@ -0,0 +1,30 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "Robot.h"
+#include <wpi/raw_ostream.h>
+
+void Robot::RobotInit() {
+  wpi::outs() << "Robot Initialized\n";
+}
+
+void Robot::RobotPeriodic() {}
+
+void Robot::AutonomousInit() {}
+void Robot::AutonomousPeriodic() {}
+
+void Robot::TeleopInit() {}
+void Robot::TeleopPeriodic() {}
+
+void Robot::DisabledInit() {}
+void Robot::DisabledPeriodic() {}
+
+void Robot::TestInit() {}
+void Robot::TestPeriodic() {}
+
+#ifndef RUNNING_FRC_TESTS
+int main() {
+  return frc::StartRobot<Robot>();
+}
+#endif
