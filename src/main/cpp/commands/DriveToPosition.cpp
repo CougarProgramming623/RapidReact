95b3d2fab75f677ce71e18917c78cf26dc3e71ea
JJ Feeney <jjfeene3rd@gmail.com>
2022-02-08 19:40:22 -0500

    Added Clock to is finished

--- src/main/cpp/commands/DriveToPosition.cpp
+++ src/main/cpp/commands/DriveToPosition.cpp
@@ -9,14 +9,17 @@ using ctre::phoenix::motorcontrol::ControlMode;
                                   m_DriveTrain.GetBackL()operation; 
 
 const int kGEARBOX_RATIO = 12;
-const int kTICKS_PER_ROTATION = 2048;  //(!) FINISH (!)
+const int kTICKS_PER_ROTATION = 2048;     //(!) FINISH (!)
 const double kMETERS_PER_ROTATION = (8.0 * 2.54) * 3.1415926 / 100.0;     //(!) FINISH (!)
-const double kCOUNT_THRESHOLD = 500;  //How close to the exact number the encoders need to be (!) Should be tested (!)
+const double kCOUNT_THRESHOLD = 500;      //How close to the exact number the encoders need to be (!) Should be tested (!)
+const double kDERIVATIVE_THRESHOLD = .01; //(!) Test (!)
+const double kTIME_THRESHOLD = 3;         //(!) TEST (!)
 
 DriveToPos::DriveToPos(double x, double y, double a){
     m_XTicks = x / kMETERS_PER_ROTATION * kTICKS_PER_ROTATION * kGEARBOX_RATIO;
 
     m_Angle = a;
+
 
 
     AddRequirements(&Robot::GetRobot()->GetDriveTrain());
@@ -49,10 +52,12 @@ void DriveToPos::Initialize(){
     m_DriveTrain.BreakMode(true);
     m_DriveTrain.UsePostionPID();
 
-    FOR_ALL_MOTORS(.ConfigPeakOutputForward(0.6, 0))
-    FOR_ALL_MOTORS(.ConfigPeakOutputReverse(-0.6, 0))
+    FOR_ALL_MOTORS(.ConfigPeakOutputForward(0.3, 0))
+    FOR_ALL_MOTORS(.ConfigPeakOutputReverse(-0.3, 0))
 
-    FOR_ALL_MOTORS(.ConfigClosedloopRamp(.02, 0))
+    FOR_ALL_MOTORS(.ConfigClosedloopRamp(.06, 0))
+
+    m_Clock.Start();
 }
 
 
@@ -61,7 +66,12 @@ bool DriveToPos::IsFinished(){
        (abs(m_DriveTrain.GetFrontL().GetSelectedSensorPosition() - (m_InitialTicks[0] + m_XTicks)) <= kCOUNT_THRESHOLD &&
         abs(m_DriveTrain.GetFrontR().GetSelectedSensorPosition() - (m_InitialTicks[1] + m_XTicks)) <= kCOUNT_THRESHOLD &&
         abs(m_DriveTrain.GetBackL().GetSelectedSensorPosition()  - (m_InitialTicks[2] + m_XTicks)) <= kCOUNT_THRESHOLD &&
-        abs(m_DriveTrain.GetBackR().GetSelectedSensorPosition()  - (m_InitialTicks[3] + m_XTicks)) <= kCOUNT_THRESHOLD);
+        abs(m_DriveTrain.GetBackR().GetSelectedSensorPosition()  - (m_InitialTicks[3] + m_XTicks)) <= kCOUNT_THRESHOLD) ||
+       (m_Clock.Get() >= (units::time::second_t) kTIME_THRESHOLD &&
+       (m_DriveTrain.GetFrontL().GetErrorDerivative() <= kDERIVATIVE_THRESHOLD ||
+        m_DriveTrain.GetFrontR().GetErrorDerivative() <= kDERIVATIVE_THRESHOLD ||
+        m_DriveTrain.GetBackL().GetErrorDerivative() <= kDERIVATIVE_THRESHOLD ||
+        m_DriveTrain.GetBackR().GetErrorDerivative() <= kDERIVATIVE_THRESHOLD));
 }
 
 void DriveToPos::Execute(){
