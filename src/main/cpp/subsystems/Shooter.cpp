9d769e7602b5ac34b52d0dbe86e86fe32f4919a3
Carson Harter <carsonharter@yahoo.com>
2022-03-05 19:08:44 -0500

    GOod

--- src/main/cpp/subsystems/Shooter.cpp
+++ src/main/cpp/subsystems/Shooter.cpp
@@ -24,11 +24,10 @@ Shooter::Shooter() :
 m_FlywheelFront(FLYWHEEL_FRONT), //master
 m_FlywheelBack(FLYWHEEL_BACK), //slave
 m_Feeder(FEEDER),
-m_FeederButton(BUTTON_L(FEEDER_BUTTON)),
-m_FlywheelToggleByDial(BUTTON_L(FLYWHEEL_BUTTON_BY_DIAL)),
-m_FlywheelToggleByDistance(BUTTON_L(FLYWHEEL_BUTTON_BY_DISTANCE)),
-m_ShootTime(BUTTON_L(16)),
-m_ReadyShoot(BUTTON_L(READYSHOOT))
+
+m_feederButton(BUTTON_L(FEEDER_BUTTON)),
+m_speedDial([&] {return Robot::GetRobot()->GetButtonBoard().GetRawAxis(0);}),
+m_shootSpeed(BUTTON_L(FLYWHEEL_BUTTON_BY_SPEED))
 {}
 
 
@@ -36,118 +35,33 @@ void Shooter::ShooterInit(){
     DebugOutF("Shooter Init");
     FeederButton();
     FlywheelButton();
-    ScaleToDistance();
-    ShootTime();
-    ShootOnReady();
+    
     m_FlywheelBack.Set(ControlMode::Follower, FLYWHEEL_FRONT);
     m_FlywheelBack.SetInverted(ctre::phoenix::motorcontrol::InvertType::OpposeMaster);
 }
 
-void Shooter::ShootOnReady(){
-    m_ReadyShoot.WhileHeld(frc2::FunctionalCommand(
-        [&]{DebugOutF("Shooting when ready");}, //OnInit
-        [&]{
-            double setpoint = m_FlywheelFront.GetClosedLoopTarget();
-            //DebugOutF(std::to_string(abs(m_FlywheelFront.GetSelectedSensorVelocity() - setpoint) * 600 / 2048));
-            if (abs(m_FlywheelFront.GetSelectedSensorVelocity() - setpoint) * 600 / 2048 <= 70
-                && m_FlywheelFront.GetSelectedSensorVelocity() * 600 / 2048 >= 100
-                && setpoint * 600 / 2048 >= 100){
-                m_Feeder.Set(ControlMode::PercentOutput, -0.5);
-                //DebugOutF("Shooting");
-            } else {
-                m_Feeder.Set(ControlMode::PercentOutput, 0);
-                //DebugOutF("Not shooting");
-            }
-        },
-        [&](bool e){m_Feeder.Set(ControlMode::PercentOutput, 0);},
-        [&]{return false;}
-    ));
-}
 
 void Shooter::FeederButton(){
-    m_FeederButton.WhenPressed(frc2::InstantCommand( [&] { 
-        m_Feeder.Set(ControlMode::PercentOutput, -0.5);
-        Robot::GetRobot()->GetCOB().GetTable().GetEntry("/COB/ifShooting").SetBoolean(true);
-
+    m_feederButton.WhenPressed(frc2::InstantCommand( [&] { 
+        m_Feeder.Set(ControlMode::PercentOutput, -1);
     }));
 
-    m_FeederButton.WhenReleased(frc2::InstantCommand( [&] { 
+    m_feederButton.WhenReleased(frc2::InstantCommand( [&] { 
         m_Feeder.Set(ControlMode::PercentOutput, 0);
-        Robot::GetRobot()->GetCOB().GetTable().GetEntry("/COB/ifShooting").SetBoolean(false);
-
     }));
 }
 void Shooter::FlywheelButton(){
+    m_shootSpeed.WhenHeld(frc2::FunctionalCommand( [&]{}, [&]{ //onExecute
+        m_FlywheelFront.Set(ControlMode::PercentOutput, (Robot::GetRobot()->GetButtonBoard().GetRawAxis(0) + 1) * 2);
+    }, [&](bool e){ //onEnd
+            m_FlywheelFront.Set(ControlMode::PercentOutput, 0);
+    }, [&]{ return false; }, {})); 
 
-    /*m_FlywheelToggle.WhenPressed(frc2::InstantCommand( [&] { 
-        DebugOutF(std::to_string((Robot::GetRobot()->GetButtonBoard().GetRawAxis(0) + 1) / 2));
-        m_FlywheelFront.Set(ControlMode::PercentOutput, ((Robot::GetRobot()->GetButtonBoard().GetRawAxis(0) + 1) / 2));
-    }));
-    */
-
-
-
-   m_FlywheelToggleByDial.WhenHeld(frc2::FunctionalCommand( [&]{}, [&]{ //onExecute
-
-            // double dialSpeedRPM = ((int)((Robot::GetRobot()->GetButtonBoard().GetRawAxis(0) + 1)  / 2 * 6001)); //rpm
-            // double targetSpeed = dialSpeedRPM / 600 * 2048; //ticks per second
-            Robot::GetRobot()->GetCOB().GetTable().GetEntry("/COB/flywheelSpeedSetpoint").SetDouble(Robot::GetRobot()->GetButtonBoard().GetRawAxis(0));
-
-                m_FlywheelFront.Set(ControlMode::PercentOutput, Robot::GetRobot()->GetButtonBoard().GetRawAxis(0));
-
-        }, [&](bool e){ //onEnd
-
-                m_FlywheelFront.Set(ControlMode::PercentOutput, 0);
-        }, [&]{ return false; }, {}
-   )); //Functional Command End
-
-        m_FlywheelToggleByDial.WhenReleased(frc2::InstantCommand( [&] { 
+    m_shootSpeed.WhenReleased(frc2::InstantCommand( [&] { 
         m_FlywheelFront.Set(ControlMode::PercentOutput, 0);
     }));
 }
 
-double Shooter::FlywheelRPM(){
-    return m_FlywheelFront.GetSelectedSensorVelocity() * 600 / 2048; //rpm
-}
-void Shooter::ShootTime(){ //button ID 13
-    m_ShootTime.WhenPressed(frc2::ParallelRaceGroup(frc2::FunctionalCommand( [&]{}, [&]{ //onExecute
-            double dialSpeed = ((int)((Robot::GetRobot()->GetButtonBoard().GetRawAxis(0) + 1)  / 2 * 6001)); //rpm
-            double targetSpeed = dialSpeed / 600 * 2048; //ticks per second
-            Robot::GetRobot()->GetCOB().GetTable().GetEntry("/COB/flywheelSpeedSetpoint").SetDouble(dialSpeed);
-
-                m_FlywheelFront.Set(ControlMode::Velocity, targetSpeed);
-
-        }, [&](bool e){ //onEnd
-
-                m_FlywheelFront.Set(ControlMode::PercentOutput, 0);
-        }, [&]{ return false; }, {}
-   ), frc2::WaitCommand(5.0_s))); //cancels after 5 seconds
-
-        m_FlywheelToggleByDial.WhenReleased(frc2::InstantCommand( [&] { 
-        m_FlywheelFront.Set(ControlMode::PercentOutput, 0);
-    }));
-}
-
-void Shooter::ScaleToDistance(){
-    m_FlywheelToggleByDistance.WhenHeld(frc2::FunctionalCommand([&] { //onInit
-
-    }, [&]{ //onExecute
-
-    double distance = Robot::GetRobot()->GetCOB().GetTable().GetEntry(COB_KEY_DISTANCE).GetDouble(0); //cm
-    double RPM = distance * 4 + 70;
-
-    double smoothRPM = runningAverage.Calculate(distance) * 4 + 70;
-
-    
-    Robot::GetRobot()->GetCOB().GetTable().GetEntry("/COB/flywheelSpeedSetpoint").SetDouble(RPM);
-
-    m_FlywheelFront.Set(ControlMode::Velocity, smoothRPM / 600 * 2048);
-
-    
-    }, [&] (bool e) { //onEnd
-    m_FlywheelFront.Set(ControlMode::PercentOutput, 0);
-
-    }, [&] {//isFinished
-        return false;
-    }, {}));
+double Shooter::FlywheelRPM() {
+    return m_FlywheelFront.GetSelectedSensorVelocity() * 600 / 2048;
 }
